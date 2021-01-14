e173ef2c6d8a0e74c08a66ba9390e153f71b2767
akashdoppalapudi <akashdoppalapudi2001@gmail.com>
2021-01-14 12:13:46 +0530

    public views changed for cgpa

--- creditsbank/apps/public/views.py
+++ creditsbank/apps/public/views.py
@@ -23,12 +23,18 @@ def profile(request: HttpRequest) -> HttpResponse:
             credits_enc = user_credits.credits
             key = user_credits.key
             credits_str = decrypt(key, credits_enc)
+            grade_pts = {'O':10, 'S':9, 'A':8, 'B':7, 'C':6, 'D':5, 'F':0, '':0}
             credit_list = [i for i in credits_str]+['' for j in range(48-len(credits_str))]
+            total_points = 0
+            for i in credit_list:
+                total_points = total_points+grade_pts[i]
+            cgpa = total_points/len(credits_str)
             user_credits = dict(zip(num, credit_list))
             obj = UserData.objects.get(user = request.user)
             context = {
             'course' : obj.course,
             'branch' : obj.branch,
+            'cgpa' : round(cgpa, 2),
             }
             context.update(user_credits)
         return render(request, 'public/profile.html', context)
